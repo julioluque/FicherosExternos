38d5b5a6a2fb58ef5c1bc2492b544b404233641f
Julio Luque <julio.a.luque@gmail.com>
2020-04-15 19:54:31 -0300

    Conexion con Stream abierta, leyendo caracteres en codigos ascii o unicode

---
+++ src/lectura/AccesoFichero.java
@@ -0,0 +1,43 @@
+package lectura;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AccesoFichero {
+
+	public static void main(String[] args) {
+
+		LeerFichero accediendoCodigosStream = new LeerFichero();
+		
+		accediendoCodigosStream.leer();
+		
+	}
+
+}
+
+class LeerFichero {
+	 
+	public void leer() {
+		
+		try {
+			FileReader entrada = new FileReader("C:/desa/apps/was/RECURSOS EXTERNOS/Streams/Ejemplo Stream.txt");
+			
+			int c = entrada.read();  //Guardamos el primer caracter en codigo entero
+			
+			while(c != -1) {
+				
+				c = entrada.read();
+				System.out.println(c);
+			}
+			
+		} catch (IOException e) {
+			System.out.println("***No se encontro el archivo... ");
+			e.printStackTrace();
+		} finally {
+			
+		}
+		
+		
+	}
+	
+}
