f6ff05ce2fa5ccc434fe37f7907ba336a93f59ad
Julio Luque <julio.a.luque@gmail.com>
2020-04-17 21:39:26 -0300

    Compatibilizado con Linux, Mac, Windows y otros

--- src/ficheros_directorios/Ficheros.java
+++ src/ficheros_directorios/Ficheros.java
@@ -4,33 +4,34 @@ import java.io.*;
 
 public class Ficheros {
 	public static void main(String[] args) {
-		
-		String ruta1 = "C:/desa/apps/was/FicherosExternos";
-		String ruta2 = "C:/desa/apps/was/RECURSOS EXTERNOS/Ficheros y Directorios";
-		
+
+		String ruta1 = "C:" + File.separator + "desa" + File.separator + "apps" + File.separator + "was"
+				+ File.separator + "FicherosExternos";
+		String ruta2 = "C:" + File.separator + "desa" + File.separator + "apps" + File.separator + "was"
+				+ File.separator + "RECURSOS EXTERNOS" + File.separator + "Ficheros y Directorios";
+
 		File rutaExterna = new File(ruta2);
-		
+
 		System.out.println("Existe? " + rutaExterna.exists());
 		System.out.println("Archivos de la carpeta: " + rutaExterna.getAbsolutePath());
 
-		String [] listaDeArchivos = rutaExterna.list();
+		String[] listaDeArchivos = rutaExterna.list();
 
 		for (String archivo : listaDeArchivos) {
 			System.out.println("\t- " + archivo);
-			
-			File rutaInterna = new File(rutaExterna.getAbsoluteFile(),archivo);
-			
+
+			File rutaInterna = new File(rutaExterna.getAbsoluteFile(), archivo);
+
 			if (rutaInterna.isDirectory()) {
-				String [] listaDeSubArchivos = rutaInterna.list();
-				
+				String[] listaDeSubArchivos = rutaInterna.list();
+
 				for (String subArchivo : listaDeSubArchivos) {
 					System.out.println("\t  - " + subArchivo);
-					
+
 				}
-				
-				
+
 			}
-			
+
 		}
 
 	}
